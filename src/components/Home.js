15c13a93e7e2301cf4f09dbc3efc3f835674b7cb
Troy Delacruz <troy8076@yahoo.com>
2023-02-05 03:40:42 -0500

    Working on Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useHistory, Link } from "react-router-dom";
 
-const Home = () => {
-  return <div>Home</div>;
-};
+function Home() {
+  const history = useHistory();
+  const handleClick = (e) => {
+    console.log(e.currentTarget.id);
+    history.push("/pizza");
+  };
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Click the button to get started!</p>
+      <Link to="/pizza">
+        <button id="order-pizza" onClick={handleClick}>
+          Order Pizza!
+        </button>
+      </Link>
+    </div>
+  );
+}
 
 export default Home;
