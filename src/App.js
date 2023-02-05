c865a167d7c7b97f5c9396f2cd4a6a620e8af5ac
Troy Delacruz <troy8076@yahoo.com>
2023-02-05 03:31:56 -0500

    Working on App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React from "react";
-
-const App = () => {
-  return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
-  );
-};
-export default App;
+import React, { useState, useEffect } from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import Pizza from "./components/Pizza";
+import Confirmation from "./components/Confirmation";
+import axios from "axios";
+import * as yup from "yup";
