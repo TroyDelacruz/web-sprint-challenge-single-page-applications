a0cc0da758e0d13534789ba7d81cf2c18e61ae9c
Troy Delacruz <troy8076@yahoo.com>
2023-02-05 03:53:10 -0500

    Finished schema

--- src/App.js
+++ src/App.js
@@ -6,7 +6,64 @@ import Confirmation from "./components/Confirmation";
 import axios from "axios";
 import * as yup from "yup";
 
+const formSchema = yup.object().shape({
+  customer: yup
+    .string()
+    .required("Name must be entered!")
+    .min(2, "Name must be at least 2 characters."),
+  size: yup
+    .string()
+    .oneOf(["Small", "Medium", "Large"], "Please select a size!"),
+  sauce: yup.string().required("Please select a sauce!"),
+  pepperoni: yup.string(),
+  sausage: yup.string(),
+  mushroom: yup.string(),
+  chicken: yup.string(),
+  ham: yup.string(),
+  extraCheese: yup.string(),
+  sub: yup.string(),
+  special: yup.string().notRequired(),
+  gluten: yup.string().notRequired(),
+});
+
+const initialFormValues = {
+  customer: "",
+  size: "",
+  sauce: "",
+  pepperoni: false,
+  sausage: false,
+  chicken: false,
+  mushroom: false,
+  ham: false,
+  extraCheese: false,
+  special: "",
+  gluten: false,
+};
+
+const initialFormErrors = {
+  size: "",
+  sauce: "",
+  customer: "",
+};
+
+const initialOrders = [];
+
+const initialDisabled = true;
+
 const App = () => {
+  const [orders, setOrders] = useState(initialOrders);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, seFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
+  function reset() {
+    setFormValues(initialFormValues);
+  }
+
+  function clearOrders() {
+    setOrders(initialOrders);
+  }
+
   return (
     <>
       <Switch>
