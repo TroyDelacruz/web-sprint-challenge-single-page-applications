47a92b006573e4c75f5db1e47864e0fbaec51686
Troy Delacruz <troy8076@yahoo.com>
2023-02-05 03:35:40 -0500

    Finish some setup of App

--- src/App.js
+++ src/App.js
@@ -5,3 +5,23 @@ import Pizza from "./components/Pizza";
 import Confirmation from "./components/Confirmation";
 import axios from "axios";
 import * as yup from "yup";
+
+const App = () => {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/pizza">
+          <Pizza />
+        </Route>
+        <Route path="/confirmation">
+          <Confirmation />
+        </Route>
+      </Switch>
+    </>
+  );
+};
+
+export default App;
